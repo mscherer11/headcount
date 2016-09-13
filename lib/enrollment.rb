4d5771b85b398966204fdae7fa2c7a201239efba
Robert Smith <robert.smith160@icloud.com>
2016-09-13 12:40:30 -0600

    updated enrollment and enrollment_test with mark

--- lib/enrollment.rb
+++ lib/enrollment.rb
@@ -1,12 +1,16 @@
 class Enrollment
+  attr_reader :name, :kindergarten_participation
+  def initialize(stuff)
+    @name = stuff[:name]
+    @kindergarten_participation = stuff[:kindergarten_participation]
+  end
 
-  def kindergarten_participation_by_year(data)
-    return data
+  def kindergarten_participation_by_year
+    kindergarten_participation
   end
 
   def kindergarten_participation_in_year(year)
-    data = {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}
-    data.fetch(year)
+    kindergarten_participation.fetch(year)
   end
 
 end
