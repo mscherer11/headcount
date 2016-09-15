e078a6d1fb591f6b7659b17c6a7321135a7da5d9
Michael Scherer <michael.c.scherer@gmail.com>
2016-09-15 16:25:53 -0600

    added the ability to load multiple files dynamically and create the necessary hash

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -9,32 +9,33 @@ class HeadcountAnalyst
   end
 
   def kindergarten_participation_rate_variation(subject1, subject2)
-    numerator_hash = sum_numerator(subject1)
-    denominator_hash = sum_denominator(subject2)
+    num_hash = sum_numerator(subject1)
+    denom_hash = sum_denominator(subject2)
 
-    numerator = numerator_hash.values.reduce(:+)/numerator_hash.length
-    denominator = denominator_hash.values.reduce(:+)/denominator_hash.length
+    numerator = num_hash.values.reduce(:+)/num_hash.length
+    denominator = denom_hash.values.reduce(:+)/denom_hash.length
 
     shorten_float(numerator/denominator)
   end
 
   def kindergarten_participation_rate_variation_trend(subject1, subject2)
-    numerator_hash = sum_numerator(subject1)
-    denominator_hash = sum_denominator(subject2)
+    num_hash = sum_numerator(subject1)
+    denom_hash = sum_denominator(subject2)
 
     trended_results = Hash.new
-    numerator_hash.each do |k,v|
-      trended_results = ({k => shorten_float(v / denominator_hash[k])}).merge!(trended_results)
+    num_hash.each do |k,v|
+      trended_results =
+      ({k => shorten_float(v / denom_hash[k])}).merge!(trended_results)
     end
     return trended_results.sort.to_h
   end
 
   def sum_numerator(subject1)
-    numerator_hash = @district_repo.find_by_name(subject1).enrollment.kindergarten_participation_by_year
+    num_hash = @district_repo.find_by_name(subject1).enrollment.kindergarten_participation_by_year
   end
 
   def sum_denominator(subject2)
-    denominator_hash = @district_repo.find_by_name(subject2[:against]).enrollment.kindergarten_participation_by_year
+    denom_hash = @district_repo.find_by_name(subject2[:against]).enrollment.kindergarten_participation_by_year
   end
 
 end
--- test/enrollment_repository_test.rb
+++ test/enrollment_repository_test.rb
@@ -26,9 +26,19 @@ class TestEnrollmentReposiotry < Minitest::Test
   def test_it_loads_kindergarten_participation
     enrollment = @repo.find_by_name("COLORADO")
     kindergarten_participation = enrollment.data[:kindergarten_participation]
-
     assert_equal 11, kindergarten_participation.length
     #test quality of the hash/use expected hash
   end
+
+  def test_can_it_load_high_school_data
+    enrollment = @repo.find_by_name("COLORADO")
+    high_school_graduation = enrollment.data[:high_school_graduation]
+    assert_equal 5, high_school_graduation.length
+  end
+
+  # def test_it_can_change_the_kindergarten_name
+  #   enrollment = @repo.find_by_name("COLORADO")
+  #   assert_equal :kindergarten_participation, enrollment.data.keys[1]
+  # end
 
 end
